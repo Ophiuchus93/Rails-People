7eef6e288e8de54e9c1c4467539053b99094d980
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-08-20 17:53:56 -0600

    Full CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_people/home'
+  get 'static_people/about'
+  root "static_people#home"
+
+  get "/about", to: "static_people#about"
+  resources :people
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
